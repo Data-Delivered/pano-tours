86242b2d1dc3594b6602cbcfaf9644bb58ed564f
DataDelivered <kevin@datadelivered.cloud>
2025-06-04 21:26:27 -0400

    2x update

--- 2460_burton/data.js
+++ 2460_burton/data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-home---parking-lot",
-      "name": "Home - Parking lot",
+      "name": "Home - Parking Lot",
       "levels": [
         {
           "tileSize": 256,
@@ -24,116 +24,72 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
         }
       ],
-      "faceSize": 3600,
+      "faceSize": 5750,
       "initialViewParameters": {
-        "yaw": -0.003971000671459279,
-        "pitch": 1.2469135595922864,
-        "fov": 1.6880274992386552
+        "yaw": 0.035579829955258546,
+        "pitch": 0.9341306157831166,
+        "fov": 1.6753017517137268
       },
       "linkHotspots": [
         {
-          "yaw": -0.27715143079162097,
-          "pitch": 0.4786591390572781,
-          "rotation": 14.922565104551524,
-          "target": "1-north---front-of-bldg"
+          "yaw": 0.041153134974205585,
+          "pitch": 0.6657205623190467,
+          "rotation": 3.141592653589793,
+          "target": "1-roof-center"
         },
         {
-          "yaw": 1.0807362424778084,
-          "pitch": 0.7449234638083837,
-          "rotation": 10.995574287564278,
-          "target": "3-southeast"
+          "yaw": 0.959242828813835,
+          "pitch": 0.7638532509788689,
+          "rotation": 4.71238898038469,
+          "target": "2-southeast"
         },
         {
-          "yaw": -0.7898838609974064,
-          "pitch": 1.1699936834822875,
-          "rotation": 7.0685834705770345,
-          "target": "5-west"
+          "yaw": -0.8568762746968801,
+          "pitch": 1.234901368960541,
+          "rotation": 19.63495408493622,
+          "target": "3-west"
         },
         {
-          "yaw": 0.20540348335841685,
-          "pitch": 1.5056259898762097,
+          "yaw": -0.7600099342559972,
+          "pitch": 0.6209660681691549,
+          "rotation": 7.853981633974483,
+          "target": "5-northwest"
+        },
+        {
+          "yaw": 0.16108934893659388,
+          "pitch": 1.4938157143681536,
           "rotation": 0,
           "target": "4-southwest"
         },
         {
-          "yaw": 0.36696489735580684,
-          "pitch": 0.4395202848486992,
+          "yaw": -0.31292141003895324,
+          "pitch": 0.49070499011349966,
+          "rotation": 8.63937979737193,
+          "target": "6-north---front-of-bldg"
+        },
+        {
+          "yaw": 0.10136994800309296,
+          "pitch": 0.4321532763371625,
+          "rotation": 3.141592653589793,
+          "target": "7-northeast"
+        },
+        {
+          "yaw": 0.4793542923405596,
+          "pitch": 0.44602748438400397,
           "rotation": 3.9269908169872414,
           "target": "8-east"
-        },
-        {
-          "yaw": 0.022112385219934083,
-          "pitch": 0.7405753217496578,
-          "rotation": 2.356194490192345,
-          "target": "2-roof-center"
-        },
-        {
-          "yaw": -0.7083588489487411,
-          "pitch": 0.6366417966228184,
-          "rotation": 7.853981633974483,
-          "target": "6-northwest"
-        },
-        {
-          "yaw": 0.09029176671215211,
-          "pitch": 0.43607171477089324,
-          "rotation": 3.141592653589793,
-          "target": "6-northwest"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-north---front-of-bldg",
-      "name": "North - Front of Bldg.",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 3600,
-      "initialViewParameters": {
-        "yaw": 0.008170037183649725,
-        "pitch": 0.5407101538404824,
-        "fov": 1.5710911379055006
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.023558250814765813,
-          "pitch": -0.6602158376052554,
-          "rotation": 9.42477796076938,
-          "target": "2-roof-center"
-        },
-        {
-          "yaw": 1.3044600304135123,
-          "pitch": 0.03236447598573022,
-          "rotation": 16.493361431346422,
-          "target": "6-northwest"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-roof-center",
+      "id": "1-roof-center",
       "name": "Roof Center",
       "levels": [
         {
@@ -156,62 +112,23 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
         }
       ],
-      "faceSize": 3600,
+      "faceSize": 5750,
       "initialViewParameters": {
-        "yaw": 0.019481020898609813,
-        "pitch": 1.5660472213660466,
-        "fov": 1.3344986255070506
+        "yaw": -0.000465573031235067,
+        "pitch": 1.569427655435506,
+        "fov": 1.3371919181249365
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.060886299171732716,
-          "pitch": 0.6579269940399008,
-          "rotation": 3.141592653589793,
-          "target": "1-north---front-of-bldg"
-        },
-        {
-          "yaw": -0.9284213850760956,
-          "pitch": 0.4706512072166351,
-          "rotation": 3.141592653589793,
-          "target": "6-northwest"
-        },
-        {
-          "yaw": -1.9992157596997089,
-          "pitch": 0.5242634179972665,
-          "rotation": 2.356194490192345,
-          "target": "5-west"
-        },
-        {
-          "yaw": -2.536387501299675,
-          "pitch": -0.016164478163785745,
-          "rotation": 3.141592653589793,
-          "target": "0-home---parking-lot"
-        },
-        {
-          "yaw": 2.613415711826031,
-          "pitch": 0.43040692065064867,
-          "rotation": 9.42477796076938,
-          "target": "3-southeast"
-        },
-        {
-          "yaw": 0.8528092836203012,
-          "pitch": 0.4634441180417781,
-          "rotation": 15.707963267948973,
-          "target": "7-northeast"
-        },
-        {
-          "yaw": 1.5728578452311517,
-          "pitch": 0.29362467875279563,
-          "rotation": 3.141592653589793,
-          "target": "8-east"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "3-southeast",
+      "id": "2-southeast",
       "name": "Southeast",
       "levels": [
         {
@@ -234,28 +151,58 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
-        }
-      ],
-      "faceSize": 3600,
-      "initialViewParameters": {
-        "yaw": -0.12261178866362243,
-        "pitch": 0.3800663768576218,
-        "fov": 1.1051180809056496
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.6393244916254908,
-          "pitch": -0.3192444580864411,
-          "rotation": 8.63937979737193,
-          "target": "0-home---parking-lot"
         },
         {
-          "yaw": -0.17533794997108387,
-          "pitch": -0.596260438569086,
-          "rotation": 3.141592653589793,
-          "target": "2-roof-center"
+          "tileSize": 512,
+          "size": 8192
         }
       ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.13356893440684914,
+        "pitch": 0.33908272723799726,
+        "fov": 1.0277161697518755
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-west",
+      "name": "West",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.3078332477485617,
+        "pitch": 0.7421071184561683,
+        "fov": 1.7002171382741535
+      },
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
@@ -282,116 +229,23 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
         }
       ],
-      "faceSize": 3600,
+      "faceSize": 5750,
       "initialViewParameters": {
-        "yaw": 0.11848592925959167,
-        "pitch": 0.43648924577852455,
-        "fov": 1.408630615946123
+        "yaw": 0.04276085586030476,
+        "pitch": 0.49892030119580966,
+        "fov": 1.1739347434757175
       },
-      "linkHotspots": [
-        {
-          "yaw": 0.06293111112260519,
-          "pitch": 0.08693815390547854,
-          "rotation": 3.141592653589793,
-          "target": "2-roof-center"
-        },
-        {
-          "yaw": -0.25865275977350244,
-          "pitch": 0.0006375089804340206,
-          "rotation": 8.63937979737193,
-          "target": "1-north---front-of-bldg"
-        },
-        {
-          "yaw": -0.7142365059908382,
-          "pitch": 0.38607555521961956,
-          "rotation": 1.5707963267948966,
-          "target": "5-west"
-        },
-        {
-          "yaw": 0.9373029785327773,
-          "pitch": 0.20807225497313908,
-          "rotation": 4.71238898038469,
-          "target": "3-southeast"
-        },
-        {
-          "yaw": 0.38849967094564875,
-          "pitch": -0.09712812008656968,
-          "rotation": 3.9269908169872414,
-          "target": "8-east"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "5-west",
-      "name": "West",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 3600,
-      "initialViewParameters": {
-        "yaw": 0.2858752979289392,
-        "pitch": 0.7219038500145487,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.2364333214516421,
-          "pitch": -0.004489940165969486,
-          "rotation": 2.356194490192345,
-          "target": "1-north---front-of-bldg"
-        },
-        {
-          "yaw": 2.5016386372767467,
-          "pitch": -0.360483493348573,
-          "rotation": 10.210176124166829,
-          "target": "0-home---parking-lot"
-        },
-        {
-          "yaw": 1.347309721070694,
-          "pitch": -0.1282134138327251,
-          "rotation": 3.9269908169872414,
-          "target": "3-southeast"
-        },
-        {
-          "yaw": 0.5549289001975382,
-          "pitch": -1.1137629987413895,
-          "rotation": 3.141592653589793,
-          "target": "2-roof-center"
-        },
-        {
-          "yaw": 2.319981121872372,
-          "pitch": 0.35222771824209786,
-          "rotation": 3.9269908169872414,
-          "target": "4-southwest"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-northwest",
+      "id": "5-northwest",
       "name": "Northwest",
       "levels": [
         {
@@ -414,40 +268,58 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
         }
       ],
-      "faceSize": 3600,
+      "faceSize": 5750,
       "initialViewParameters": {
-        "yaw": 0.020568871600627148,
-        "pitch": 0.7803424305007312,
-        "fov": 1.6880274992386552
+        "yaw": -0.016875075843069354,
+        "pitch": 0.6463008502564609,
+        "fov": 1.44493168099151
       },
-      "linkHotspots": [
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-north---front-of-bldg",
+      "name": "North - Front of Bldg.",
+      "levels": [
         {
-          "yaw": -0.03436818134119868,
-          "pitch": -0.7215787131074034,
-          "rotation": 3.141592653589793,
-          "target": "2-roof-center"
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": 0.9832718209822815,
-          "pitch": -0.5365942347271222,
-          "rotation": 10.210176124166829,
-          "target": "0-home---parking-lot"
+          "tileSize": 512,
+          "size": 512
         },
         {
-          "yaw": 1.0269652059927523,
-          "pitch": 0.11499868538667002,
-          "rotation": 4.71238898038469,
-          "target": "5-west"
+          "tileSize": 512,
+          "size": 1024
         },
         {
-          "yaw": -1.227029043668363,
-          "pitch": 0.015475941866618825,
-          "rotation": 7.853981633974483,
-          "target": "1-north---front-of-bldg"
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
         }
       ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.0035994311191061,
+        "pitch": 0.6230687811131475,
+        "fov": 1.6640238897414101
+      },
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
@@ -474,28 +346,19 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
-        }
-      ],
-      "faceSize": 3600,
-      "initialViewParameters": {
-        "yaw": 0.08112533107585307,
-        "pitch": 0.6432080149177377,
-        "fov": 1.6880274992386552
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.6830385728944481,
-          "pitch": 0.09781016479007221,
-          "rotation": 4.71238898038469,
-          "target": "1-north---front-of-bldg"
         },
         {
-          "yaw": 0.2267901521024811,
-          "pitch": -0.8733895152199942,
-          "rotation": 3.141592653589793,
-          "target": "2-roof-center"
+          "tileSize": 512,
+          "size": 8192
         }
       ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.2687836796318841,
+        "pitch": 0.6646661096980608,
+        "fov": 1.4680966396945725
+      },
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
@@ -522,54 +385,27 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
         }
       ],
-      "faceSize": 3600,
+      "faceSize": 5750,
       "initialViewParameters": {
-        "yaw": -0.9147565344002082,
-        "pitch": 0.9426612672424657,
-        "fov": 1.5707963267948966
+        "yaw": -0.8663584053348536,
+        "pitch": 0.9189784532424543,
+        "fov": 1.600554518832659
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.2814183316730947,
-          "pitch": 0.17499819272693706,
-          "rotation": 10.210176124166829,
-          "target": "1-north---front-of-bldg"
-        },
-        {
-          "yaw": -1.561475308650099,
-          "pitch": 0.140936304575634,
-          "rotation": 2.356194490192345,
-          "target": "4-southwest"
-        },
-        {
-          "yaw": -0.970099029130342,
-          "pitch": -0.32902884064008475,
-          "rotation": 3.141592653589793,
-          "target": "2-roof-center"
-        },
-        {
-          "yaw": -1.991197355523271,
-          "pitch": -0.26669605847300915,
-          "rotation": 3.141592653589793,
-          "target": "0-home---parking-lot"
-        },
-        {
-          "yaw": -1.0775010411174701,
-          "pitch": 0.09866359354147036,
-          "rotation": 3.141592653589793,
-          "target": "5-west"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "2460 Burton Street",
+  "name": "2460 Burton",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
